ed84e1a0e3b51c72cfb3de6abf124fc99e0fcf6d
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-19 00:09:54 -0700

    Fix build by compiling exo implementations

--- src-kernel/exo/exo_disk.c
+++ src-kernel/exo/exo_disk.c
@@ -1,22 +1,53 @@
 #include "kernel/exo_disk.h"
 #include "defs.h"
+#include "fs.h"
+#include "sleeplock.h"
+#include "buf.h"
 
-(void)n;
-  return -1;
-=======
-int exo_read_disk(struct exo_blockcap cap, void *dst, uint64_t off, uint64_t n) {
-int __attribute__((weak)) exo_read_disk(struct exo_blockcap cap, void *dst, uint64_t off, uint64_t n) {
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-    // TODO: implement disk read via capability
-    (void)cap; (void)dst; (void)off; (void)n;
-    return -1;
+int exo_read_disk(struct exo_blockcap cap, void *dst, uint64_t off, uint64_t n)
+{
+  struct buf b;
+  uint64_t tot = 0;
+  memset(&b, 0, sizeof(b));
+  initsleeplock(&b.lock, "exodisk");
+
+  while (tot < n) {
+    uint64_t cur = off + tot;
+    struct exo_blockcap blk = { cap.dev, cap.blockno + cur/BSIZE };
+    size_t m = MIN(n - tot, BSIZE - cur % BSIZE);
+
+    acquiresleep(&b.lock);
+    exo_bind_block(&blk, &b, 0);
+    memmove((char *)dst + tot, b.data + cur % BSIZE, m);
+    releasesleep(&b.lock);
+
+    tot += m;
+  }
+
+  return (int)tot;
 }
 
+int exo_write_disk(struct exo_blockcap cap, const void *src, uint64_t off, uint64_t n)
+{
+  struct buf b;
+  uint64_t tot = 0;
+  memset(&b, 0, sizeof(b));
+  initsleeplock(&b.lock, "exodisk");
 
-int __attribute__((weak)) exo_write_disk(struct exo_blockcap cap, const void *src, uint64_t off, uint64_t n) {
+  while (tot < n) {
+    uint64_t cur = off + tot;
+    struct exo_blockcap blk = { cap.dev, cap.blockno + cur/BSIZE };
+    size_t m = MIN(n - tot, BSIZE - cur % BSIZE);
 
-    // TODO: implement disk write via capability
-    (void)cap; (void)src; (void)off; (void)n;
-    return -1;
+    acquiresleep(&b.lock);
+    memmove(b.data + cur % BSIZE, (char *)src + tot, m);
+    exo_bind_block(&blk, &b, 1);
+    releasesleep(&b.lock);
 
+    tot += m;
+  }
+
+  return (int)tot;
 }
