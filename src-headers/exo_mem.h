edfdc5591ea35bff8dbba8e30b48fbcf61a8fb38
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-18 23:42:58 -0700

    Restructure sources into src-kernel and src-uland

--- src-headers/exo_mem.h
+++ src-headers/exo_mem.h
@@ -1,5 +1,9 @@
 #pragma once
+<<<<<<<< HEAD:src-headers/exo_mem.h
 #include "exo.h"
+========
+#include "../../exo.h"
+>>>>>>>> fd61ec6 (Restructure sources into src-kernel and src-uland):src-kernel/kernel/exo_mem.h
 
 exo_cap exo_alloc_page(void);
 int exo_unbind_page(exo_cap cap);
