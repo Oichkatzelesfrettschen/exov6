23dd1496c7d06534982bfa6fd7b5d1fa91a5a530
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-20 02:13:34 -0700

    Add gas budget management for exokernel tasks

--- caplib.h
+++ caplib.h
@@ -10,6 +10,8 @@ int cap_flush_block(exo_blockcap *cap, void *data);
 int cap_send(exo_cap dest, const void *buf, uint64 len);
 int cap_recv(exo_cap src, void *buf, uint64 len);
 int cap_set_timer(void (*handler)(void));
+int cap_set_gas(uint64 amount);
+int cap_get_gas(void);
 void cap_yield_to(context_t **old, context_t *target);
 int cap_yield_to_cap(exo_cap target);
 int cap_read_disk(exo_cap cap, void *dst, uint64 off, uint64 n);
