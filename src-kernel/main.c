6edf66968b5f43aaefc16528bebb88f97ef634f9
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-20 22:29:29 -0700

    Clean up DAG scheduler code

--- src-kernel/main.c
+++ src-kernel/main.c
@@ -7,7 +7,6 @@
 #include "dag.h"
 #include "x86.h"
 #include "exo_stream.h"
-#include "dag.h"
 
 static void startothers(void);
 static void mpmain(void)  __attribute__((noreturn));
@@ -40,10 +39,6 @@ main(void)
   fileinit();      // file table
   ideinit();       // disk
   dag_sched_init(); // initialize DAG scheduler
-  initialize DAG scheduler
-  static struct exo_stream stream = {0};
-  dag_sched_init(&stream);
-  exo_stream_register(&stream);
   startothers();   // start other processors
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
