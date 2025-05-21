1291842ed68fbd0f6c4fd3a6254ec1915d675885
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-20 22:32:34 -0700

    Clean up exo.h prototypes

--- exo.h
+++ exo.h
@@ -2,7 +2,6 @@
 #define EXO_H
 #include "types.h"
 
-
 typedef struct hash256 {
   uint64 parts[4];
 } hash256_t;
@@ -13,7 +12,6 @@ typedef struct exo_cap {
   uint rights;
   uint owner;
   hash256_t auth_tag;
-
 } exo_cap;
 
 typedef struct exo_blockcap {
@@ -25,8 +23,6 @@ typedef struct exo_blockcap {
 
 exo_cap exo_alloc_page(void);
 int exo_unbind_page(exo_cap c);
-exo_cap cap_new(uint id, uint rights, uint owner);
-int cap_verify(exo_cap c);
 exo_cap cap_new(uint id, uint rights, uint owner);
 int cap_verify(exo_cap c);
 
