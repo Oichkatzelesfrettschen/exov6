bd37ce3e6ca847f97195061ea0d1d8bba7ffffba
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-22 14:50:27 -0700

    Add user-space DAG scheduling API and demo

--- src-headers/src-uland/libos/sched.h
+++ src-headers/src-uland/libos/sched.h
@@ -1,8 +1,32 @@
 #pragma once
 #include "types.h"
-#include "user.h"
 #include "caplib.h"
 
 void sched_add(exo_cap cap);
 void sched_install_timer(void);
 void sched_run(void);
+
+struct dag_node;
+
+/* DAG scheduling helpers.  Nodes can be given a weight and
+ * dependencies.  The simple user-space implementation orders ready
+ * nodes by descending weight. */
+struct dag_node_edge {
+  struct dag_node *child;
+  struct dag_node_edge *next;
+};
+
+struct dag_node {
+  exo_cap ctx;
+  int weight;
+  int pending;
+  int done;
+  struct dag_node_edge *children;
+  struct dag_node *next;
+};
+
+void dag_node_init(struct dag_node *n, exo_cap ctx);
+void dag_node_set_weight(struct dag_node *n, int weight);
+void dag_node_add_dep(struct dag_node *parent, struct dag_node *child);
+void dag_sched_submit(struct dag_node *node);
+void dag_sched_yield(void);
--- src-uland/user/dag_demo.c
+++ src-uland/user/dag_demo.c
@@ -1,62 +1,30 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include "libos/sched.h"
-
-typedef struct exo_cap { uint32_t pa; } exo_cap;
-
-struct dag_node { int pending; int priority; int weight; };
-
-static void dag_node_init(struct dag_node *n, exo_cap ctx) {
-  (void)ctx;
-  n->pending = 0;
-  n->priority = 0;
-  n->weight = 1;
-}
-static void dag_node_add_dep(struct dag_node *parent, struct dag_node *child) {
-  (void)parent; (void)child;
-}
-static void dag_sched_submit(struct dag_node *node) {
-  printf("dag_sched_submit prio %d weight %d\n", node->priority, node->weight);
-}
-static void exo_stream_yield(void) {
-  printf("exo_stream_yield called\n");
-}
-static void beatty_sched_set_tasks(exo_cap a, exo_cap b) {
-  (void)a; (void)b; printf("beatty_sched_set_tasks called\n");
-}
-static void beatty_sched_set_weights(int a, int b) {
-  printf("beatty_sched_set_weights %d %d\n", a, b);
-}
+#include "src-uland/libos/sched.h"
 
 static struct dag_node a, b, c;
 
 static void setup(void) {
   exo_cap cap = {0};
   dag_node_init(&a, cap);
-  dag_node_set_priority(&a, 10);
-  dag_node_set_weight(&a, 3);
+  dag_node_set_weight(&a, 10);
   dag_node_init(&b, cap);
-  dag_node_set_priority(&b, 5);
-  dag_node_set_weight(&b, 1);
+  dag_node_set_weight(&b, 5);
   dag_node_init(&c, cap);
-  dag_node_set_priority(&c, 1);
-  dag_node_set_weight(&c, 2);
+  dag_node_set_weight(&c, 1);
   dag_node_add_dep(&a, &c);
   dag_node_add_dep(&b, &c);
   dag_sched_submit(&a);
   dag_sched_submit(&b);
   dag_sched_submit(&c);
-  beatty_sched_set_tasks(cap, cap);
-  beatty_sched_set_weights(3, 1);
 }
 
 int main(int argc, char *argv[]) {
-  (void)argc; (void)argv;
+  (void)argc;
+  (void)argv;
   printf("DAG scheduler demo\n");
   setup();
-  exo_stream_yield();
-  exo_stream_yield();
-  exo_stream_yield();
+  dag_sched_yield();
+  dag_sched_yield();
+  dag_sched_yield();
   return 0;
 }
