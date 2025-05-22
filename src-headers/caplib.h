435896a5e2a6501e1966d850ea90f0284b57fd33
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-22 12:34:19 -0700

    Move all header files into src-headers

--- src-headers/caplib.h
+++ src-headers/caplib.h
@@ -1,1 +1,23 @@
-../caplib.h
+#pragma once
+#include "exo.h"
+#include "exo_cpu.h"
+#include "include/exokernel.h"
+
+exo_cap cap_alloc_page(void);
+int cap_unbind_page(exo_cap cap);
+int cap_alloc_block(uint dev, uint rights, exo_blockcap *cap);
+int cap_bind_block(exo_blockcap *cap, void *data, int write);
+void cap_flush_block(exo_blockcap *cap, void *data);
+int cap_send(exo_cap dest, const void *buf, uint64 len);
+int cap_recv(exo_cap src, void *buf, uint64 len);
+int cap_set_timer(void (*handler)(void));
+int cap_set_gas(uint64 amount);
+int cap_get_gas(void);
+int cap_out_of_gas(void);
+void cap_yield_to(context_t **old, context_t *target);
+int cap_yield_to_cap(exo_cap target);
+int cap_read_disk(exo_blockcap cap, void *dst, uint64 off, uint64 n);
+int cap_write_disk(exo_blockcap cap, const void *src, uint64 off, uint64 n);
+int cap_ipc_echo_demo(void);
+int cap_inc(uint16_t id);
+int cap_dec(uint16_t id);
