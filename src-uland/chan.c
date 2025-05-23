9824b2740415321d6eeb979b1c37663f62baeb60
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-23 16:07:55 -0700

    docs: clarify typed channel workflow

--- src-uland/chan.c
+++ src-uland/chan.c
@@ -22,16 +22,22 @@ chan_destroy(chan_t *c)
 int
 chan_endpoint_send(chan_t *c, exo_cap dest, const void *msg, size_t len)
 {
-    if(len != c->msg_size)
+    if(len != c->msg_size){
+        printf(2, "chan_endpoint_send: size %d != %d\n", (int)len,
+               (int)c->msg_size);
         return -1;
+    }
     return cap_send(dest, msg, c->msg_size);
 }
 
 int
 chan_endpoint_recv(chan_t *c, exo_cap src, void *msg, size_t len)
 {
-    if(len != c->msg_size)
+    if(len != c->msg_size){
+        printf(2, "chan_endpoint_recv: size %d != %d\n", (int)len,
+               (int)c->msg_size);
         return -1;
+    }
     return cap_recv(src, msg, c->msg_size);
 }
 
