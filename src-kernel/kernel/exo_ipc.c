698220f3b2cc448943ea7b1173821bca8e11b239
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-23 10:22:05 -0700

    Refactor arch setup and fix headers

--- src-kernel/kernel/exo_ipc.c
+++ src-kernel/kernel/exo_ipc.c
@@ -4,23 +4,15 @@
 
 static struct exo_ipc_ops ipc_ops;
 
-void
-exo_ipc_register(struct exo_ipc_ops *ops)
-{
-  ipc_ops = *ops;
-}
+void exo_ipc_register(struct exo_ipc_ops *ops) { ipc_ops = *ops; }
 
-int
-exo_send(exo_cap dest, const void *buf, uint64_t len)
-{
+int exo_send(exo_cap dest, const void *buf, uint64_t len) {
   if (ipc_ops.send)
     return ipc_ops.send(dest, buf, len);
   return -1;
 }
 
-int
-exo_recv(exo_cap src, void *buf, uint64_t len)
-{
+int exo_recv(exo_cap src, void *buf, uint64_t len) {
   if (ipc_ops.recv)
     return ipc_ops.recv(src, buf, len);
   return -1;
