3f3fa66154d53daa06c2b6b33b679dc2bff31576
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-23 15:24:49 -0700

    Copy newline-suffixed headers

--- src-headers/exo.h
+++ src-headers/exo.h
@@ -1,1 +1,31 @@
-../exo.h
+#ifndef EXO_H
+#define EXO_H
+#include "types.h"
+
+typedef struct hash256 {
+  uint64 parts[4];
+} hash256_t;
+
+typedef struct exo_cap {
+  uint pa;
+  uint id;
+  uint rights;
+  uint owner;
+  hash256_t auth_tag;
+} exo_cap;
+
+typedef struct exo_blockcap {
+  uint dev;
+  uint blockno;
+  uint rights;
+  uint owner;
+} exo_blockcap;
+
+#ifdef EXO_KERNEL
+exo_cap exo_alloc_page(void);
+int exo_unbind_page(exo_cap c);
+exo_cap cap_new(uint id, uint rights, uint owner);
+int cap_verify(exo_cap c);
+#endif
+
+#endif // EXO_H
