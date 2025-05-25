239d6e765bfbec21368c6b708fe0bbf4080b23b2
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-24 19:25:42 -0700

    build: gate spinlock sources by CONFIG_SMP

--- src-kernel/rspinlock.c
+++ src-kernel/rspinlock.c
@@ -1,28 +1,27 @@
 #include "rspinlock.h"
+#include <config.h>
 
 struct cpu;
 
-extern struct cpu* mycpu(void);
+extern struct cpu *mycpu(void);
 extern void pushcli(void);
 extern void popcli(void);
-extern void acquire(struct spinlock*);
-extern void release(struct spinlock*);
-extern int holding(struct spinlock*);
-extern void panic(char*);
+extern void acquire(struct spinlock *);
+extern void release(struct spinlock *);
+extern int holding(struct spinlock *);
+extern void panic(char *);
 
-void
-rinitlock(struct rspinlock *rlk, char *name)
-{
+#if CONFIG_SMP
+
+void rinitlock(struct rspinlock *rlk, char *name) {
   rlk->owner = 0;
   rlk->depth = 0;
   initlock(&rlk->lk, name);
 }
 
-void
-racquire(struct rspinlock *rlk)
-{
+void racquire(struct rspinlock *rlk) {
   pushcli();
-  if(rlk->owner == mycpu()){
+  if (rlk->owner == mycpu()) {
     rlk->depth++;
     return;
   }
@@ -31,25 +30,33 @@ racquire(struct rspinlock *rlk)
   rlk->depth = 1;
 }
 
-void
-rrelease(struct rspinlock *rlk)
-{
-  if(rlk->owner != mycpu() || rlk->depth < 1)
+void rrelease(struct rspinlock *rlk) {
+  if (rlk->owner != mycpu() || rlk->depth < 1)
     panic("rrelease");
   rlk->depth--;
-  if(rlk->depth == 0){
+  if (rlk->depth == 0) {
     rlk->owner = 0;
     release(&rlk->lk);
   }
   popcli();
 }
 
-int
-rholding(struct rspinlock *rlk)
-{
+int rholding(struct rspinlock *rlk) {
   int r;
   pushcli();
   r = (rlk->owner == mycpu());
   popcli();
   return r;
 }
+#else
+void rinitlock(struct rspinlock *rlk, char *name) {
+  (void)rlk;
+  (void)name;
+}
+void racquire(struct rspinlock *rlk) { (void)rlk; }
+void rrelease(struct rspinlock *rlk) { (void)rlk; }
+int rholding(struct rspinlock *rlk) {
+  (void)rlk;
+  return 1;
+}
+#endif
