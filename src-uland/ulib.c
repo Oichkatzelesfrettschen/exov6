a4603960f6c0c39fdb3eb42a52f0b2203e437e24
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-25 01:28:06 -0700

    Use libc memory helpers

--- src-uland/ulib.c
+++ src-uland/ulib.c
@@ -2,105 +2,77 @@
 #include "stat.h"
 #include "fcntl.h"
 #include "user.h"
-#include "x86.h"
+#include <string.h>
 
-char*
-strcpy(char *s, const char *t)
-{
+char *strcpy(char *s, const char *t) {
   char *os;
 
   os = s;
-  while((*s++ = *t++) != 0)
+  while ((*s++ = *t++) != 0)
     ;
   return os;
 }
 
-int
-strcmp(const char *p, const char *q)
-{
-  while(*p && *p == *q)
+int strcmp(const char *p, const char *q) {
+  while (*p && *p == *q)
     p++, q++;
   return (uint8_t)*p - (uint8_t)*q;
 }
 
-size_t
-strlen(const char *s)
-{
+size_t strlen(const char *s) {
   int n;
 
-  for(n = 0; s[n]; n++)
+  for (n = 0; s[n]; n++)
     ;
   return n;
 }
 
-void*
-memset(void *dst, int c, size_t n)
-{
-  stosb(dst, c, n);
-  return dst;
-}
+void *memset(void *dst, int c, size_t n) { return __builtin_memset(dst, c, n); }
 
-char*
-strchr(const char *s, char c)
-{
-  for(; *s; s++)
-    if(*s == c)
-      return (char*)s;
+char *strchr(const char *s, char c) {
+  for (; *s; s++)
+    if (*s == c)
+      return (char *)s;
   return 0;
 }
 
-char*
-gets(char *buf, size_t max)
-{
+char *gets(char *buf, size_t max) {
   size_t i, cc;
   char c;
 
-  for(i=0; i+1 < max; ){
+  for (i = 0; i + 1 < max;) {
     cc = read(0, &c, 1);
-    if(cc < 1)
+    if (cc < 1)
       break;
     buf[i++] = c;
-    if(c == '\n' || c == '\r')
+    if (c == '\n' || c == '\r')
       break;
   }
   buf[i] = '\0';
   return buf;
 }
 
-int
-stat(const char *n, struct stat *st)
-{
+int stat(const char *n, struct stat *st) {
   int fd;
   int r;
 
   fd = open(n, O_RDONLY);
-  if(fd < 0)
+  if (fd < 0)
     return -1;
   r = fstat(fd, st);
   close(fd);
   return r;
 }
 
-int
-atoi(const char *s)
-{
+int atoi(const char *s) {
   int n;
 
   n = 0;
-  while('0' <= *s && *s <= '9')
-    n = n*10 + *s++ - '0';
+  while ('0' <= *s && *s <= '9')
+    n = n * 10 + *s++ - '0';
   return n;
 }
 
-void*
-memmove(void *vdst, const void *vsrc, size_t n)
-{
-  char *dst;
-  const char *src;
-
-  dst = vdst;
-  src = vsrc;
-  while(n-- > 0)
-    *dst++ = *src++;
-  return vdst;
+void *memmove(void *vdst, const void *vsrc, size_t n) {
+  return __builtin_memmove(vdst, vsrc, n);
 }
