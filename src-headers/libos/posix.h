75850523a1e6345fb49288285049140ecea636a0
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-25 04:03:14 -0700

    Add additional POSIX stubs

--- src-headers/libos/posix.h
+++ src-headers/libos/posix.h
@@ -16,3 +16,31 @@ int libos_fork(void);
 int libos_waitpid(int pid);
 int libos_sigsend(int pid, int sig);
 int libos_sigcheck(void);
+
+/* Additional POSIX helpers */
+struct stat;
+typedef unsigned long libos_sigset_t;
+
+int libos_stat(const char *path, struct stat *st);
+long libos_lseek(int fd, long off, int whence);
+void *libos_mmap(void *addr, size_t len, int prot, int flags, int fd, long off);
+int libos_munmap(void *addr, size_t len);
+
+int libos_sigemptyset(libos_sigset_t *set);
+int libos_sigfillset(libos_sigset_t *set);
+int libos_sigaddset(libos_sigset_t *set, int sig);
+int libos_sigdelset(libos_sigset_t *set, int sig);
+int libos_sigismember(const libos_sigset_t *set, int sig);
+
+int libos_getpgrp(void);
+int libos_setpgid(int pid, int pgid);
+
+struct sockaddr;
+typedef unsigned socklen_t;
+int libos_socket(int domain, int type, int protocol);
+int libos_bind(int fd, const struct sockaddr *addr, socklen_t len);
+int libos_listen(int fd, int backlog);
+int libos_accept(int fd, struct sockaddr *addr, socklen_t *len);
+int libos_connect(int fd, const struct sockaddr *addr, socklen_t len);
+long libos_send(int fd, const void *buf, size_t len, int flags);
+long libos_recv(int fd, void *buf, size_t len, int flags);
--- src-uland/user/libos_posix_extra_test.c
+++ src-uland/user/libos_posix_extra_test.c
@@ -1,11 +1,24 @@
 #include "libos/posix.h"
 #include "user.h"
+#include "stat.h"
+#include "signal.h"
 
 int main(void) {
     int fd = libos_open("extra", 0);
     if (fd < 0)
         fd = libos_open("extra", O_CREATE);
     libos_write(fd, "x", 1);
+    libos_lseek(fd, 0, 0);
+    struct stat st;
+    libos_stat("extra", &st);
+    void *p = libos_mmap(0, 128, 0, 0, -1, 0);
+    libos_munmap(p, 128);
+    libos_getpgrp();
+    libos_setpgid(0, 0);
+    libos_socket(0, 0, 0);
+    libos_sigset_t ss;
+    libos_sigemptyset(&ss);
+    libos_sigaddset(&ss, SIGUSR1);
     libos_close(fd);
     char *argv[] = {"echo", "extra", 0};
     libos_spawn("echo", argv);
