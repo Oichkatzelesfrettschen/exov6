b78a4668e03574206c19fd93337c41e8e2377603
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-24 18:52:51 -0700

    Switch docs to CMake and clang

--- src-headers/x86.h
+++ src-headers/x86.h
@@ -5,14 +5,46 @@ static inline uchar
 inb(ushort port)
 {
   uchar data;
-  asm volatile("inb %1,%0" : "=a"(data) : "d"(port));
+  __asm__ volatile("inb %1,%0" : "=a"(data) : "d"(port));
   return data;
 }
 
 static inline void
 outb(ushort port, uchar data)
 {
-  asm volatile("outb %0,%1" : : "a"(data), "d"(port));
+  __asm__ volatile("outb %0,%1" : : "a"(data), "d"(port));
+}
+
+static inline void
+insl(int port, void *addr, int cnt)
+{
+  __asm__ volatile("cld; rep insl" :
+                   "=D"(addr), "=c"(cnt) : "d"(port), "0"(addr), "1"(cnt) :
+                   "memory");
+}
+
+static inline void
+outsl(int port, const void *addr, int cnt)
+{
+  __asm__ volatile("cld; rep outsl" :
+                   "=S"(addr), "=c"(cnt) : "d"(port), "0"(addr), "1"(cnt) :
+                   "memory");
+}
+
+static inline void
+stosb(void *addr, int data, int cnt)
+{
+  __asm__ volatile("cld; rep stosb" :
+                   "=D"(addr), "=c"(cnt) : "0"(addr), "1"(cnt), "a"(data) :
+                   "memory");
+}
+
+static inline void
+stosl(void *addr, int data, int cnt)
+{
+  __asm__ volatile("cld; rep stosl" :
+                   "=D"(addr), "=c"(cnt) : "0"(addr), "1"(cnt), "a"(data) :
+                   "memory");
 }
 
 #endif // XV6_X86_H
