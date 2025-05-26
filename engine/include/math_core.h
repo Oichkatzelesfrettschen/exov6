4d9886df53f0562389eb452ab7977f506f3ffe1e
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-26 03:03:08 -0700

    Add fib_big with _BitInt support

--- engine/include/math_core.h
+++ engine/include/math_core.h
@@ -12,3 +12,10 @@ uint64_t fib_big(uint32_t n);
 #endif
 uint64_t gcd(uint64_t a, uint64_t b);
 size_t phi_align(size_t n);
+
+#ifdef __BITINT_MAXWIDTH__
+typedef unsigned _BitInt(256) fib_big_t;
+fib_big_t fib_big(uint32_t n);
+#else
+uint64_t fib_big(uint32_t n);
+#endif
