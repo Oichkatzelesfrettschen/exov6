d40f8717e2870367b757d9bfaf3044608a97ed28
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-26 19:24:29 -0700

    x86: register MMX cap

--- arch/x86/legacy/simd_mmx_cap.c
+++ arch/x86/legacy/simd_mmx_cap.c
@@ -1,4 +1,5 @@
 #include <mmintrin.h>
+#include "../../simd_dispatch.h"
 
 int cap_validate_mmx(void) {
   __m64 z = _mm_setzero_si64();
@@ -10,4 +11,9 @@ void dag_process_mmx(void) {
   __m64 b = _mm_add_pi16(a, a);
   (void)b;
   _mm_empty();
+}
+
+__attribute__((constructor))
+static void register_mmx(void) {
+  simd_register(SIMD_FEATURE_MMX, cap_validate_mmx, dag_process_mmx);
 }
