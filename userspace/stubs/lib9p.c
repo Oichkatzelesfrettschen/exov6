94a8cc334d4d6eb1353b30ceb1caf27585ecfcd1
Eirikr Hinngart <151315375+Oichkatzelesfrettschen@users.noreply.github.com>
2025-05-30 01:26:51 -0700

    Expand stub libraries with host implementations

--- userspace/stubs/lib9p.c
+++
@@ -1,2 +0,0 @@
-/* Minimal stub source to ensure library builds */
-[[maybe_unused]] void lib9p_stub(void) {}
--- userspace/stubs/libposix.c
+++
@@ -1,2 +0,0 @@
-/* Minimal stub source to ensure library builds */
-[[maybe_unused]] void libposix_stub(void) {}
